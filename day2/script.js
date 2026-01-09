6bb4060f99966391e1c5b640a52a1bdf15a4ef98
Co3code <countian3@gmail.com>
2026-01-09 21:00:38 +0800

    temparture converter and grade calulator example

--- day2/script.js
+++ day2/script.js
@@ -24,3 +24,30 @@ function introduceStuden(name, age, subject) {
 }
 introduceStuden("anthony", 20, "computer scicece");
 introduceStuden("cyan", 22, "Mathematics");
+
+function calculateArea(length, width) {
+  const area = length * width;
+  return area; // send result back
+}
+
+const roomArea = calculateArea(10, 5);
+console.log(`Room are: ${roomArea} square meters`);
+
+console.log(`small area :${calculateArea(3, 3)} square meters`);
+
+//practical example
+
+// tempartor converter
+function celsiusToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+function calculateGrade(score, maxScore) {
+  const percentage = (score / maxScore) * 100;
+  return percentage;
+}
+
+console.log("25°C =", celsiusToFahrenheit(25), "°F");
+console.log("score  18/20", calculateGrade(18, 20), "%");
+
